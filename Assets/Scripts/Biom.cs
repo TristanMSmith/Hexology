c2243e0566eb1328dec323489f5e36355788833a
TristanMSmith <30475619+TristanMSmith@users.noreply.github.com>
2017-07-26 13:40:30 -0400

    Add files via upload

---
+++ Assets/Scripts/Biom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biom : MonoBehaviour {
+    public string[] resources;
+    //public string[] 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addResource() {
+
+    }
+}
---
+++ Assets/Scripts/MakeWorld.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeWorld : MonoBehaviour {
+    public int WorldRadius;
+    public GameObject[] Bioms;
+    public int radius;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject MakeRandomTile(GameObject optNeighboringBiom) {
+        if (optNeighboringBiom) { }
+        //Random r = new Random();
+        int r = (int)Random.Range(0, Bioms.Length - 1);
+        //int rInt = r.Next(0, Bioms.Length-1); //for ints
+        return Bioms[r];
+    }
+
+    public List<float>[][] TileCoordinates(int radius) {
+        List<float>[][] result;
+        int x, y;
+        while (radius >= 0) {
+            x = --radius;
+        }
+
+        return result;
+    }
+}
